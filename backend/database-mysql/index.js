35de1940c4008b203fffd592b614fecc83f77692
JasserGa <jessergafsi2@gmail.com>
2024-02-01 11:08:25 +0300

    ...

--- backend/database-mysql/index.js
+++ backend/database-mysql/index.js
@@ -4,5 +4,6 @@ const config = require('./config');
 // Create MySQL connection pool
 const pool = mysql.createPool(config);
 
+
 // Export the pool for use in other modules
 module.exports = pool.promise();
