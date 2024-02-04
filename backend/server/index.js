75b66fe5dd3dd27247d9a38d46739a20ffacc573
AmirOuerghi <ouerghi501@gmail.com>
2024-02-04 15:17:24 +0100

    ffffff

--- backend/server/index.js
+++ backend/server/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const emailjs = require('emailjs-com');
-
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
